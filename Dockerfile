dffdf8996005b7740971072029fbe5852f7158a8
Sree Polavarapu <sree.polavarapu@amp.co.nz>
2019-12-23 10:58:55 +1300

    Req #docker - compose with networking

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.5
-RUN pip install Flask==0.11.1
+RUN pip install Flask==0.11.1 redis==3.2.1
 RUN useradd -ms /bin/bash admin
 USER admin
 WORKDIR /app
--- app/app.py
+++ app/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, render_template
+import redis
 
 app = Flask(__name__)
 default_key = '1'
-cache = {default_key: 'one'}
+cache = redis.Redis(host='redis', port=6379, db=0)
 
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
@@ -12,11 +13,11 @@ def mainpage():
 	    key = request.form['key']
 
 	if request.method == 'POST' and request.form['submit'] == 'save':
-		cache[key] = request.form['cache_value']
+		cache.set(key, request.form['cache_value'])
 
 	cache_value = None;
-	if key in cache:
-		cache_value = cache[key]
+	if cache.get(key):
+		cache_value = cache.get(key)
 
 	return render_template('index.html', key=key, cache_value=cache_value)
 
